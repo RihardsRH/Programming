e4b4b7358eadc287cc380d79db9f52dce722eb0f
RihardsRH <114136031+RihardsRH@users.noreply.github.com>
2022-10-06 10:51:56 +0300

    Add files via upload

--- astronoms.py
+++ astronoms.py
@@ -1,7 +1,14 @@
 import random
 horoskopi = ["Auns", "Vērsis", "Dvīņi", "Vēzis", "Lauva", "Jaunava", "Svari", "Skorpions", "Strēlnieks", "Mežāzis", "Ūdensvīrs", "Zivis"]
-a1 = ["ap11","ap12","ap13","ap14","ap15","ap16","ap17","ap18","ap19","ap120","ap121","ap122"]
-a2 = ["ap21","ap22","ap23","ap24","ap25","ap26","ap27","ap28","ap29","ap230","ap231","ap232"]
+a1 = ["Centieties visu novērtēt lietišķi,nepakļaujieties emocijām","Svārstīsieties vai uzstādīt stingrākas prasības līdzcilvēkiem.",
+"Šodien lieli panākumi nav gaidāmi,bet varat izvairīties no zaudējuma.",
+"Kādu ideju vēlēsieties īstenot bez kavēšanās, tomēr ieteicas neteigies.","Uzņemieties iniciatīvu kāda darbaveikšanai.",
+"Nebūsiet gatavs kompromisiem, tasapgrūtinās lēmumu pieņemšanu.","Ļaujiet notikumiem notikt dabiski,nesteidziniet tos.","Varat iegūt jaunus sadarbības partnerus.",
+"Nepieļaujiet vieglprātību nevienā jomā.","Savus nodomus pārvērtējiet vēl un vēlreiz.",
+"Neļaujieties īgnumam. ","Gādājiet par mājas miera uzturēšanu."]
+a2 = ["Par palīgiem izvēlieties cilvēkus, kuri ir jums uzticami.","Esiet izlēmīgs, bet ne sīkumains.",
+"Apmeklējiet vecākus radiniekus.","Nepieprasiet no iemīļotās personas, lai viņa apmierina visas jūsu vēlmes.",
+"Neuztveriet jebkuru apgalvojumu par augstāko patiesību, spriediet un salīdziniet pats.","ap26","ap27","ap28","ap29","ap230","ap231","ap232"]
 a3 = ["ap31","ap32","ap33","ap34","ap35","ap36","ap37","ap38","ap39","ap310","ap311","ap312"]
 r1 = ["ri1","ri2","ri3","ri4","ri5","ri6","ri7","ri8","ri9","ri10","ri11","ri12"]
 for i in range(len(horoskopi)):
