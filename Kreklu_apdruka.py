44d27cffe904d78bab11ef71ee3683c7295fdf77
RihardsRH <114136031+RihardsRH@users.noreply.github.com>
2022-10-12 16:30:31 +0300

    Pabeigts.

--- Kreklu_apdruka.py
+++ Kreklu_apdruka.py
@@ -1,23 +1,23 @@
 def krekli(skaits,apdruka,piegade):
     c = 0
-    if apdruka == 1:
-        apdruka = 5
-        cena = apdruka*skaits
-    elif apdruka == 2:
-        apdruka = 7
-        cena = apdruka*skaits
-    elif apdruka == 3:
-        apdruka = 20
-        cena = apdruka*skaits
-    if piegade == 4 and cena <= 50:
-        c = cena + 15
-    if piegade == 5:
-        c = c +cena
-    if c > 100:
-        ce = c*95/100
-        print(ce)
+    if apdruka == 1:#ja teik iedadīts "1" tad izvēlas teksta apdrukuy
+        apdruka = 5#teksta apdrukas cena
+        cena = apdruka*skaits#tiek aprēķināta cena ar noteikto kreklu daudzumu
+    elif apdruka == 2:#ja  tiek ievadits "2" tad izvēlas zīmes apdruku
+        apdruka = 7#zīmes apdrukas cena
+        cena = apdruka*skaits#tiek aprēķināta cena ar noteikto kreklu daudzumu
+    elif apdruka == 3:# ja tiek ievadīts "3" tad izvēlas ar foto apdruku
+        apdruka = 20#foto apdurkas cena
+        cena = apdruka*skaits#tiek aprēķināta cena ar noteikto kreklu daudzumu
+    if piegade == 4 and cena <= 50:# ja izvēlēta piegāde ievada "4" un ja cena ir zem 50Eur tad cenai pieskaitīs 15Eur
+        c = cena + 15#cenas aprēkāšana ar piegādes cenu
+    if piegade == 5:# ja piegāde neizvēlēta tad ievada "5"
+        c = c +cena#cena bez piegādes
+    if c > 100:#ja cena pārsniedz 100Eur 
+        ce = c*95/100#tiek aprēķināta 5% atlaide noteiktajai cenai
+        print(ce)#izprintē gala cenu
     else:
-        print(c)
+        print(c)#izprintē gala cenu
 
 #skaits = int(input("Ievadiet kreklu skaitu:"))
 #apdruka = int(input("Ievadi apdrukas veidu (1-teksts I 2-zime I 3-foto):"))
