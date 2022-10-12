bdb28008c9d08e2eb4ee047545642121a26cc99d
RihardsRH <114136031+RihardsRH@users.noreply.github.com>
2022-10-12 16:22:45 +0300

    Pabeigts.

--- Kreklu_apdruka.py
+++ Kreklu_apdruka.py
@@ -1,33 +1,28 @@
-#def krekli(skaits,apdruka,piegade):
+def krekli(skaits,apdruka,piegade):
+    c = 0
+    if apdruka == 1:
+        apdruka = 5
+        cena = apdruka*skaits
+    elif apdruka == 2:
+        apdruka = 7
+        cena = apdruka*skaits
+    elif apdruka == 3:
+        apdruka = 20
+        cena = apdruka*skaits
+    if piegade == 4 and cena <= 50:
+        c = cena + 15
+    if piegade == 5:
+        c = c +cena
+    if c > 100:
+        ce = c*95/100
+        print(ce)
+    else:
+        print(c)
 
-skaits = int(input("Ievadiet kreklu skaitu:"))
-apdruka = int(input("Ievadi apdrukas veidu (1-teksts I 2-zime I 3-foto):"))
-piegade = input("piegādi vajag(1) vai ne(2):")
-if apdruka == 1:
-    apdruka = 5
-    cena = apdruka*skaits
-elif apdruka == 2:
-    apdruka = 7
-    cena = apdruka*skaits
-elif apdruka == 3:
-    apdruka = 20
-    cena = apdruka*skaits
-if piegade == True and cena <= 50:
-    cena =cena + 15
-print(cena)
-if piegade == False:
-    print(cena)
+#skaits = int(input("Ievadiet kreklu skaitu:"))
+#apdruka = int(input("Ievadi apdrukas veidu (1-teksts I 2-zime I 3-foto):"))
+#piegade = int(input("piegādi vajag(1) vai ne(2):"))
     
-    
-
-
-
-
-
-
-
-
-
 
 #krekli(skaits,apdruka,piegade)
-
+krekli(10,3,5)#manuāli ievada attiecīgos definīcijas datus
